5542a9d4f131e1f9960c4432e9947eb6ce1d1d8e
Ephraim Arizona <degreatarizona@gmail.com>
2024-10-26 16:33:25 +0100

    Update Invoice.js

--- js/Invoice.js
+++ js/Invoice.js
@@ -1,19 +1,19 @@
 
-//const btn = document.getElementById("openInvoiceBtn");
+const btn = document.getElementById("openInvoiceBtn");
 const span = document.getElementsByClassName("close")[0];
+const modal = document.getElementById("invoiceModal");
 
-function openInvoiceBtn() {
-    const modal = document.getElementById("invoiceModal");
+
+btn.onclick = function() {
     modal.style.display = "block";
 }
 
 span.onclick = function() {
-    const modal = document.getElementById("invoiceModal");
     modal.style.display = "none";
 }
 
+
 window.onclick = function(event) {
-    const modal = document.getElementById("invoiceModal");
     if (event.target == modal) {
         modal.style.display = "none";
     }
